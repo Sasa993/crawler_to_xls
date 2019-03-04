de4df80cf5b0ceea9f7406e696050591c4105359
sasa993 <jovicic.sasa@hotmail.com>
2019-03-04 04:59:11 +0100

    it going through every page and appending the list with every phone number. next, I need to check if there is a certain number and print the details of that post

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.common.exceptions import NoSuchElementException    
+from selenium.webdriver.common.by import By
 
 class Stan(unittest.TestCase):
 
@@ -9,27 +12,36 @@ class Stan(unittest.TestCase):
         self.driver = webdriver.Chrome()
 
     def test_one(self):
+    	lista = []
+
     	driver = self.driver
     	driver.get("https://www.malioglasi.com")
-    	# grupa_oglasa = driver.find_element_by_xpath("//select[@name='grupar']")
-    	# options_grupa_oglasa = grupa_oglasa.find_elements_by_tag_name("option")
+
     	grupa_oglasa = Select(driver.find_element_by_name("grupar"))
     	grupa_oglasa.select_by_visible_text("Stanovi")
 
     	tip_oglasa = Select(driver.find_element_by_name("tipr"))
     	tip_oglasa.select_by_visible_text("Izdavanje")
-#066 497 795
+
     	grad = Select(driver.find_element_by_name("gradr"))
     	grad.select_by_visible_text("Banjaluka")
-
-    	# driver.find_element_by_value("Pretraži").click()
+    	
     	driver.find_element_by_xpath("//input[@value='Pretraži']").click()
 
-    	broj_telefona = "262-557"
-    	kita = driver.find_elements_by_css_selector("span.crveni")
-    	for x in kita:
-    		if (broj_telefona in x.text):
-    			print("YEAH! Evo ga: " + str(x.text))
+    	broj_telefona = "497-795"
+
+    	while(True):
+    		try:
+    			driver.find_element_by_xpath("//img[@src='../nlimages/or.gif']").click()
+    			svi_brojevi_telefona = driver.find_elements_by_css_selector("span.crveni")
+
+		    	for x in svi_brojevi_telefona:
+		    		lista.append(x.text)
+
+    		except NoSuchElementException:
+    			break
+
+    	print(lista)
 
     def tearDown(self):
         self.driver.close()
